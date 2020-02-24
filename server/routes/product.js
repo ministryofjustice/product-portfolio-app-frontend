32724884891550dbf9d4019f548dcdd81ee76207
Steven Wilson <steven@dominion79.co.uk>
2020-02-24 19:07:06 +0000

    Add product tpl
    - Add product route
    - Add product tpl
    - Add refact product tpl to use gov design system

---
+++ server/routes/product.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+module.exports = function product () {
+  const router = express.Router()
+  router.get('/product/:id', (req, res) => {
+    console.log('GET product')
+    res.render('pages/product', {
+      // product: product.getById(req.params.id)
+    })
+  })
+  return router
+}
