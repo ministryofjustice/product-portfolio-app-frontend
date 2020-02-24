0e1e89f949e99f5dd68e396f5d35b853c07daa08
Steven Wilson <steven@dominion79.co.uk>
2020-02-24 19:07:26 +0000

    Add product route to app

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const sassMiddleware = require('node-sass-middleware')
 
 const createIndexRouter = require('./routes/index')
+const createProductRouter = require('./routes/product')
 
 module.exports = function createApp () { // eslint-disable-line no-shadow
   const app = express()
@@ -32,5 +33,6 @@ module.exports = function createApp () { // eslint-disable-line no-shadow
   app.use(express.static(path.join(__dirname, 'public')))
   app.use('/assets', express.static(path.join(__dirname, 'node_modules/govuk-frontend/govuk/assets')))
   app.use('/', createIndexRouter())
+  app.use('/', createProductRouter())
   return app
 }
