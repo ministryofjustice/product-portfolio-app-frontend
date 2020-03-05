e1a01749b44e3280709c56a43bd959a8e66b7b13
Steven Wilson <steven@dominion79.co.uk>
2020-03-05 10:20:35 +0000

    Add better error handling to product service

--- server/services/products.js
+++ server/services/products.js
@@ -1,10 +1,14 @@
 module.exports = function products (contentfulClient) {
   async function getContent (contentType) {
-    const entries = await contentfulClient.getEntries({
-      content_type: contentType
-    })
-    if (entries.items) return entries.items
-    console.log(`Error getting Entries for ${contentType}.`)
+    try {
+      const entries = await contentfulClient.getEntries({
+        content_type: contentType
+      })
+      if (entries.items) return entries.items
+      console.log(`Error getting Entries for ${contentType}.`)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return {
