d4cbff13bd1116b4b94b9cb8392ecdac960f75e9
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-05-19 14:56:09 +0100

    Sort new rich text fields
    - Add contentful richtext module
    - Add formatting for rich text fields
    - Rename product owner to product manager

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const richTextRenderer = require('@contentful/rich-text-html-renderer')
 
 module.exports = function product ({ productService }) {
   const router = express.Router()
   router.get('/product/:id', async (req, res) => {
     const product = await productService.getEntry(req.params.id)
     console.log('GET product')
+    product.fields.productOutcomes = richTextRenderer.documentToHtmlString(product.fields.productOutcomes)
+    product.fields.productRelatedProductsOrServices = richTextRenderer.documentToHtmlString(product.fields.productRelatedProductsOrServices)
+    product.fields.productExampleFeatures = richTextRenderer.documentToHtmlString(product.fields.productExampleFeatures)
     res.render('pages/product', {
       product: product.fields
     })
