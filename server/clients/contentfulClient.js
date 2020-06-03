a54c6e3eb19921509bef874afe92eea0d28e6d05
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-06-03 15:17:03 +0100

    Debugging container

--- server/clients/contentfulClient.js
+++ server/clients/contentfulClient.js
@@ -1,5 +1,7 @@
-const contentful = require('contentful');
 const config = require('../config');
+const contentful = require('contentful');
+
+console.log(config);
 
 const contentfulClient = contentful.createClient({
   space: config.space,
