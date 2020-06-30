70e2ea03f9238319a71641f46fa9c6b81ffd660b
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2020-06-30 11:51:55 +0100

    Fix display name for Phases
    - Move status data into globals
    - Update index to use globals
    - Update product to use globals

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
-const { formatRawProductData, formatRawThemeData } = require('../utils');
+const { formatRawProductData, formatRawThemeData, statuses } = require('../utils');
 
 module.exports = function index({ productService }) {
   const router = express.Router();
@@ -8,7 +8,6 @@ module.exports = function index({ productService }) {
     const rawProducts = await productService.getEntries('product');
     const rawThemes = await productService.getEntries('themes');
     const products = formatRawProductData(rawProducts);
-    const statuses = ['backlog', 'discovery', 'alpha', 'private-beta', 'beta', 'live'];
     const themes = formatRawThemeData(rawThemes);
     console.log('GET index');
     res.render('pages/index', {
